0fade5660be541df79d91e0063563f28fc001997
6513169 <Nitiwadee.lim@student.mahidol.edu>
2025-05-04 23:39:35 +0700

    ิีสร้าง home

--- frontend/proj-web/src/router/index.js
+++ frontend/proj-web/src/router/index.js
@@ -1,35 +1,22 @@
 import {createRouter, createWebHistory} from "vue-router"
-import Users from "../views/Users.vue"
-import AddUser from "../views/AddUser.vue"
-import UpdateUser from "../views/UpdateUser.vue"
 import SignIn from "../views/SignIn.vue"
 import SignUp from "../views/SignUp.vue"
 import Cal from "../views/Cal.vue"
+import Home from "../views/Home.vue"
 import { getAuth } from "firebase/auth";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes:[
     {
-        path: "/users",
-        name: "Users",
-        component: Users,
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
-        path: "/adduser",
-        name: "AddUser",
-        component: AddUser,
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
         path: "/signin",
         name: "SignIn",
         component: SignIn
+    },
+    {
+        path: "/home",
+        name: "home",
+        component: Home
     },
     {
         path: "/signup",
@@ -42,20 +29,12 @@ const router = createRouter({
         component: Cal
     },
     {
-        path: "/updateuser/:userId",
-        name: "UpdateUser",
-        component: UpdateUser,
-        meta: {
-            requiresAuth: true
-        }
-    },
-    {
         path: '/:catchAll(.*)',
-        redirect: '/signin'
+        redirect: '/home'
     },
     {
         path: '/',
-        redirect: '/signin'
+        redirect: '/home'
     }
     ]
 })
