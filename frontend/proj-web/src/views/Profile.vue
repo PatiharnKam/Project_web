dd870656bba59f1a357de66e41d96db349b2c8da
PatiharnKam <patiharn.kam@gmail.com>
2025-05-06 16:00:25 +0700

    Update Profile.vue

--- frontend/proj-web/src/views/Profile.vue
+++ frontend/proj-web/src/views/Profile.vue
@@ -164,7 +164,7 @@ export default {
           gender: data.Gender || '',
           age: data.Age || '',
           activity: data.Activity || '',
-          bodyFat: data.Fat_Percent || '',
+          bodyFat: data.bodyFat || '',
           goal: data.Goal || ''
         };
       } catch (error) {
