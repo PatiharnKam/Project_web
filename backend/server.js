1f019e9d55112aba50fa063bbcc11f8459db08ac
PatiharnKam <patiharn.kam@gmail.com>
2025-05-12 00:30:14 +0700

    test

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB:', mongoose.connection.name);
+    console.log("asfsdagdsag")
 });
 mongoose.connection.on('error', (err) => {
     console.log('MongoDB connection error:', err);
